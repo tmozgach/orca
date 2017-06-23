9510615272a25389c7162424e1b903bd7286eaa1
tmozgach <tmozgach@sfu.ca>
2017-06-22 17:06:01 -0700

    Update test.sh

--- c/test.sh
+++ c/test.sh
@@ -1,3 +1,10 @@
 sleep 5
-echo "Hello :)"
-exit 0
+# The bug with `quast`, have to test twice!
+brew test quast
+
+if brew test $(brew list) --cc=gcc-4.8 | grep -q ': failed'; then
+  echo "Tests failed!"
+  exit 1
+else
+  brew tests
+fi
